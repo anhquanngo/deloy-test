8c53f17f6a23d165faa1a249277b3eb0b7cfa7ca
quananh <anhquan31299@gmail.com>
2020-04-11 17:27:09 +0700

    Updates

---
+++ precache-manifest.47f1c70f9716ae46c1a9f1fbd38bd04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be72bd0cc9114af235e5dbd8294ea861",
+    "url": "/deloy-test/index.html"
+  },
+  {
+    "revision": "581c6bfb73ce7462c6ca",
+    "url": "/deloy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1365ab5856c4467d8d5a",
+    "url": "/deloy-test/static/js/2.080ff1cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deloy-test/static/js/2.080ff1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581c6bfb73ce7462c6ca",
+    "url": "/deloy-test/static/js/main.682048cc.chunk.js"
+  },
+  {
+    "revision": "851425b10184e034e5b6",
+    "url": "/deloy-test/static/js/runtime-main.b08e62ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deloy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
